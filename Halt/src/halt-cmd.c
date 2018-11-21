4cde630b1e567ed9dc59f5e52c187e8bcb882a9c
tarang <tarangp@danlawinc.com>
2018-11-21 15:19:39 +0530

    2nd v2.0 updated

--- Halt/src/halt-cmd.c
+++ Halt/src/halt-cmd.c
@@ -6,7 +6,7 @@ int main()
 {
 //	FILE *fp;
 //	char pm_hm_SSL[50] = "\nSSL_ID 3 . System Going to Halt Mode\n";
-	printf("in halt v1.0\n");
+	printf("in halt v2.0\n");
 	system("/usr/bin/halt_cmd_nyc.sh");
 /*	fp = fopen("/usr/bin/tmpSSL.txt","a+");
 	if(fp == NULL)
